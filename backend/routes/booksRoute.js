66ec723e2948592304443aebec70f74346d9bc20
James Espinosa <de.james013@gmail.com>
2024-08-05 18:09:20 +0800

    bind

--- backend/routes/booksRoute.js
+++ backend/routes/booksRoute.js
@@ -2,11 +2,12 @@ import express from 'express';
 import BookController from '../controllers/BookController.js';
 
 const booksRouter = express.Router();
+const bookController = new BookController();
 
-booksRouter.get('/', new BookController().index);
-booksRouter.get('/:id', new BookController().show);
-booksRouter.post('/', new BookController().store);
-booksRouter.put('/:id', new BookController().update);
-booksRouter.delete('/:id', new BookController().destroy);
+booksRouter.get('/', bookController.index.bind(bookController));
+booksRouter.get('/:id', bookController.show.bind(bookController));
+booksRouter.post('/', bookController.store.bind(bookController));
+booksRouter.put('/:id', bookController.update.bind(bookController));
+booksRouter.delete('/:id', bookController.destroy.bind(bookController));
 
 export default booksRouter;
