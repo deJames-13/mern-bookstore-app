87ee2e58d5e8b9faf76b0a572076b4cb249a072d
James Espinosa <de.james013@gmail.com>
2024-08-09 15:35:49 +0800

    refer

--- frontend/src/components/Form/FloatingInput.jsx
+++ frontend/src/components/Form/FloatingInput.jsx
@@ -1,21 +1,20 @@
 import { propTypes } from './config';
 
-function FloatingInput(props) {
+function FloatingInput({ label, refer, ...inputProps }) {
   // eslint-disable-next-line no-unused-vars
-  const { label, refer, errorMessages, ...rest } = props;
 
   return (
     <div className='relative border border-gray-500 rounded-lg overflow-clip'>
       <input
-        {...rest}
-        ref={refer}
         type='text'
         className='block shadow-xl px-2.5 pb-2.5 pt-5 w-full text-sm bg-base-200 border-0 border-gray-300 appearance-none  focus:border-b-2 focus:outline-none focus:ring-0 focus:border-primary/80 peer'
         placeholder=' '
         autoComplete='off'
+        ref={refer}
+        {...inputProps}
       />
       <label
-        htmlFor={props.id ?? 'floating__filled'}
+        htmlFor={inputProps.id ?? 'floating__filled'}
         className='absolute text-sm duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-primary/80 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto'
       >
         {label}
--- frontend/src/components/Form/Input.jsx
+++ frontend/src/components/Form/Input.jsx
@@ -1,11 +1,9 @@
 import { propTypes } from './config';
-function Input(props) {
-  // eslint-disable-next-line no-unused-vars
-  const { label, refer, errorMessages, ...rest } = props;
+function Input({ label, refer, ...inputProps }) {
   return (
     <label className='flex items-center gap-2 input input-bordered'>
       <span className='text-gray-400 text-opacity-60'>{label ?? ''}</span>
-      <input type='text' className='grow' ref={refer} {...rest} />
+      <input type='text' className='grow' ref={refer} {...inputProps} />
     </label>
   );
 }
